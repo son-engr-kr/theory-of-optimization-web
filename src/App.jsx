495db71c18f4db2bfa80b177865ce8bbfa1953d3
Hyoungseo Son <son.engr.kr@gmail.com>
2025-10-16 17:56:45 -0400

    lazy loading for large page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import MainPage from './components/MainPage'
-import Theorem228 from './components/Theorem228'
-import Lemma270 from './components/Lemma270'
+
+const Theorem228 = lazy(() => import('./components/Theorem228'))
+const Lemma270 = lazy(() => import('./components/Lemma270'))
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/theorem-2.28" element={<Theorem228 />} />
-          <Route path="/lemma-2.70" element={<Lemma270 />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/theorem-2.28" element={<Theorem228 />} />
+            <Route path="/lemma-2.70" element={<Lemma270 />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   )
